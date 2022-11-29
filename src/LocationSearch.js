f34ce8bcdb80d4f5599f042b07f2a22313dff4de
halania104 <halaniap@outlook.com>
2022-11-29 12:36:17 -0500

    Format fix on Location Search

--- src/LocationSearch.js
+++ src/LocationSearch.js
@@ -3,33 +3,34 @@ import "./Weather.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 
-
 export default function LocationSearch() {
-function findingPlace(city) {
-  let apiKey = "4ea07c27d25d25e1861d7e9cc4008ce7";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=imperial`;
-  axios.get(apiUrl).then(showWeather);
+  function findingPlace(city) {
+    let apiKey = "4ea07c27d25d25e1861d7e9cc4008ce7";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=imperial`;
+    axios.get(apiUrl).then(showWeather);
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    let city = document.querySelector("#search-input").value;
+    findingPlace(city);
+  }
+
+  function searchLocation(position) {
+    let apiKey = "4ea07c27d25d25e1861d7e9cc4008ce7";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${apiKey}&units=imperial`;
+
+    axios.get(apiUrl).then(showWeather);
+  }
+
+  function getCurrentLocation(event) {
+    event.preventDefault();
+    navigator.geolocation.getCurrentPosition(searchLocation);
+  }
+
+  return (
+    <p>
+      <LocationSearch />
+    </p>
+  );
 }
-
-function handleSubmit(event) {
-  event.preventDefault();
-  let city = document.querySelector("#search-input").value;
-  findingPlace(city);
-}
-
-function searchLocation(position) {
-  let apiKey = "4ea07c27d25d25e1861d7e9cc4008ce7";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${apiKey}&units=imperial`;
-
-  axios.get(apiUrl).then(showWeather);
-}
-
-function getCurrentLocation(event) {
-  event.preventDefault();
-  navigator.geolocation.getCurrentPosition(searchLocation);
-}
-};
- return (
-
- )
-
